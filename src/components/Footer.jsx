cb502dad305e5b7d5129305e5f58657920008ec0
Konrad <konradbieniek@gmail.com>
2023-05-04 23:46:05 +0200

    first components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+	return (
+		<footer>
+			<p>
+				© copyright by{" "}
+				<a href='https://business-card-pan-be.netlify.app/'>pan.be</a>
+				{" " + new Date().getFullYear()}
+			</p>
+		</footer>
+	)
+}
+
+export default Footer
---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+const Note = () => {
+	return (
+		<div className='note'>
+			<h1>Note one</h1>
+			<p>Content</p>
+		</div>
+	)
+}
+
+export default Note
