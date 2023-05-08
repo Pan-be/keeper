8e08a576515f002ec4bf9d26dad99bf0056ad717
Konrad <konradbieniek@gmail.com>
2023-05-08 22:08:45 +0200

    delete noteHandler

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react"
+
 const Note = (props) => {
+	const [note, setNote] = useState(true)
+
+	const deleteNoteHandler = () => {
+		setNote(false)
+	}
+
 	return (
-		<div className='note'>
-			<h1>{props.title}</h1>
-			<p>{props.content}</p>
-		</div>
+		note && (
+			<div className='note'>
+				<h1>{props.title}</h1>
+				<p>{props.content}</p>
+				<button onClick={deleteNoteHandler}>X</button>
+			</div>
+		)
 	)
 }
 
